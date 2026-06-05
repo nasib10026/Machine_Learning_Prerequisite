0a16082cfcf65b5607d6cf48153e1c5a07c0deee
nasib10026 <nasib1026@gmail.com>
2026-06-06 00:47:57 +0600

    practiced list and basics

---
+++ python/main.py
@@ -0,0 +1,30 @@
+# print("We are learning Python!")
+
+# a = 10
+
+# if a > 10:
+#     print("a is greater than 10") 
+# else:
+#     print("a is not greater than 10")
+
+# import random
+
+# random_number = random.randint(1,10);
+
+
+
+# count = 0
+
+# while True:
+#  guess = int(input("Guess a number between 1 and 10: "))
+#  count += 1
+#  if guess == random_number:
+#     print(f"Congratulations! You guessed the number in {count} attempts.")
+#     break
+#  elif guess < random_number:
+#     print("Your guess is too low.Try again")
+#  else:
+#     print("Your guess is too high.")
+
+lst = []
+print(type(lst))
